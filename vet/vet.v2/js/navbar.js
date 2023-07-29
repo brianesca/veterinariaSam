280257eca71b9ed964d35a47fc685fc54b40b1ce
Manuel Gomez <jmgc@virket.com>
2023-07-29 02:47:53 -0600

    homologando barra de navegacion y productos dinamicos

---
+++ vet/vet.v2/js/navbar.js
@@ -0,0 +1,109 @@
+const headerDiv = document.querySelector('.one-header');
+
+// Contenido HTML que deseas insertar
+const contenidoHTML = `
+<nav class="sidebar">
+<header>
+    <div class="scroll-header">
+        <div class="header-title">
+            <button id="toggleSidebar" class="desktop toggleSidebar">☰</button>
+            <div class="header-logo">
+                <a href="index.html">
+                    <img src="Imageneshtml/LogoSAM2.png" alt="Logo">
+                </a>
+            </div>
+        </div>
+        <a href="carrito.html" class="header-button desktop">
+            <img src="Imageneshtml/shopping-cart.svg" alt="Carrito" >
+        </a>
+        <div class="menu-icon" onclick="toggleMenu()">
+            <a href="sesion.html" class="header-button desktop">
+                <span class="nombreDeUsuario"></span>
+                <img src="Imageneshtml/usuario.png" alt="Usuario">
+            </a>
+        </div>
+    </div>
+   
+    <div class="navbar mobile">
+        <button id="toggleSidebarMobile" class="toggleSidebar">☰</button>
+        <a href="carrito.html" class="header-button">
+            <img src="Imageneshtml/carritoo.png" alt="Carrito">
+        </a>
+        <div class="menu-icon" onclick="toggleMenu()">
+            <a href="sesion.html" class="header-button">
+                <span class="nombreDeUsuario"></span>
+                <img src="Imageneshtml/usuario.png" alt="Usuario">
+            </a>
+        </div>
+    </div>
+
+</header>
+
+
+<ul>
+    <li class="active nav-option-general"><a href="catalogoProductos.html">Productos</a></li>
+    <li class="active nav-option-general"><a href="contacto.html">Contacto</a></li>
+    <li class="active nav-option-general"><a href="citas.html">Agendar Cita</a></li>
+    <li class="active nav-option-general"><a href="somos.html">¿Quienes Somos?</a></li>
+    <li class="active nav-option-general"><a href="Dudas.html">Preguntas frecuentes</a></li>
+</ul>
+</nav>
+`;
+
+// Usar insertAdjacentHTML() para agregar el contenido HTML dentro del div "catalog"
+headerDiv.insertAdjacentHTML('afterbegin', contenidoHTML);
+
+
+
+//***** TEAM ******/
+
+    // Este we se encarga de que el header se minimice al bajar o navegar en la pagina
+    window.addEventListener('scroll', function () {
+        var header = document.querySelector('.scroll-header');
+        if (window.scrollY > 100) {
+            header.classList.add('small');
+        } else {
+            header.classList.remove('small');
+        }
+    });
+
+    // este es pa cuando se haga clic fuera del banner, este se cierre auto
+    document.addEventListener('click', function (event) {
+        var sidebar = document.querySelector('.sidebar');
+        var targetElement = event.target;
+
+        if (!sidebar.contains(targetElement) && targetElement.id !== 'toggleSidebar') {
+            sidebar.classList.remove('open');
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+(() => {
+
+    // Agregar el evento a todos los botones con la clase "toggleSidebar"
+    const toggleButtons = document.querySelectorAll('.toggleSidebar');
+    const sidebar = document.querySelector('.sidebar');
+    toggleButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            sidebar.classList.toggle('open');
+        });
+    });
+
+    // Cargar y Mostrar Usuario
+    function documentReady() {
+        const misElementos = document.getElementsByClassName('nombreDeUsuario');
+        for (let i = 0; i < misElementos.length; i++) {
+            misElementos[i].textContent = localStorage.getItem("nombre");
+        }
+    }
+    document.addEventListener('DOMContentLoaded', documentReady);
+})();
---
+++ vet/vet.v2/js/product-list.js
@@ -0,0 +1,82 @@
+const productos = [
+    {
+      id: 1,
+      img: "",
+      nombre: 'Collar 1',
+      marca:"dogCat",
+      precio: 10.99,
+      descripcion: 'Descripción del Collar 1'
+    },
+    {
+      id: 2,
+      img: "",
+      nombre: 'Collar 2',
+      marca:"dogCat",
+      precio: 25.00,
+      descripcion: 'Descripción del Collar 2'
+    },
+    {
+      id: 3,
+      img: "",
+      nombre: 'Collar 3',
+      marca:"dogCat",
+      precio: 5.99,
+      descripcion: 'collar para perro raza pequeña 3'
+    },
+    {
+      id: 4,
+      img: "",
+      nombre: 'Collar 4',
+      marca:"dogCat",
+      precio: 5.99,
+      descripcion: 'collar para perro raza pequeña 4'
+    },
+    {
+      id: 5,
+      img: "",
+      nombre: 'Collar 5',
+      marca:"dogCat",
+      precio: 5.99,
+      descripcion: 'collar para perro raza pequeña 5'
+    },
+    {
+      id: 6,
+      img: "",
+      nombre: 'Collar 6',
+      marca:"dogCat",
+      precio: 5.99,
+      descripcion: 'collar para perro raza pequeña 6'
+    },
+    {
+      id: 7,
+      img: "",
+      nombre: 'Collar 7',
+      marca:"dogCat",
+      precio: 5.99,
+      descripcion: 'collar para perro raza pequeña 7'
+    },
+    {
+      id: 8,
+      img: "",
+      nombre: 'Collar 8',
+      marca:"dogCat",
+      precio: 5.99,
+      descripcion: 'collar para perro raza pequeña 8'
+    },
+    {
+      id: 9,
+      img: "",
+      nombre: 'Collar 9',
+      marca:"dogCat",
+      precio: 5.99,
+      descripcion: 'collar para perro raza pequeña 9'
+    },
+    {
+      id: 10,
+      img: "",
+      nombre: 'Collar 10',
+      marca:"dogCat",
+      precio: 5.99,
+      descripcion: 'collar para perro raza pequeña 10'
+    }
+  ];
