e9fe9485e7b841a9150893f056b7978701c09789
formaegby <139494287+formaegby@users.noreply.github.com>
2023-07-30 12:57:51 -0500

    cambios img Se agrego las img y desc

--- js/product-list.js
+++ js/product-list.js
@@ -1,82 +1,82 @@
 const productos = [
     {
       id: 1,
-      img: "./Imageneshtml/IMG_3264.jpg",
-      nombre: 'Collar y shampoo para pulgas',
-      marca:"dogCat",
-      precio: 10.99,
-      descripcion: 'Collar y producto para pulgas'
+      img: "./Imageneshtml/IMAGEN1.png",
+      nombre: 'ACTICEF',
+      marca:"DVB",
+      precio: 150,
+      descripcion: 'Desparasitante para cualquier tipo de animal o mascota'
     },
     {
       id: 2,
-      img: "./Imageneshtml/logoWA.png",
-      nombre: 'Collar 2',
-      marca:"dogCat",
-      precio: 25.00,
-      descripcion: 'Descripción del Collar 2'
+      img: "./Imageneshtml/IMAGEN 3.jpg",
+      nombre: 'URINARY CARE',
+      marca:"EatDog",
+      precio: 80,
+      descripcion: 'Alimento enlatado para perros.'
     },
     {
       id: 3,
-      img: "./Imageneshtml/logoWA.png",
-      nombre: 'Collar 3',
-      marca:"dogCat",
-      precio: 5.99,
-      descripcion: 'collar para perro raza pequeña 3'
+      img: "./Imageneshtml/IMAGEN 4.jpg",
+      nombre: 'PURINA ONE',
+      marca:"PURINA",
+      precio: 30,
+      descripcion: 'Alimento en sobre para gatos.'
     },
     {
       id: 4,
-      img: "./Imageneshtml/logoWA.png",
-      nombre: 'Collar 4',
-      marca:"dogCat",
-      precio: 5.99,
-      descripcion: 'collar para perro raza pequeña 4'
+      img: "./Imageneshtml/IMAGEN 5.jpg",
+      nombre: 'DENTAL FRESH',
+      marca:"Medical Proven",
+      precio: 70,
+      descripcion: 'Enjuague bucal especial para el cuidado de las mascotas.'
     },
     {
       id: 5,
-      img: "./Imageneshtml/logoWA.png",
-      nombre: 'Collar 5',
-      marca:"dogCat",
-      precio: 5.99,
-      descripcion: 'collar para perro raza pequeña 5'
+      img: "./Imageneshtml/IMAGEN 6.jpeg",
+      nombre: 'Collar RP',
+      marca:"DIXIE",
+      precio: 100,
+      descripcion: 'collar repelente parasitos para gatos'
     },
     {
       id: 6,
-      img: "./Imageneshtml/logoWA.png",
-      nombre: 'Collar 6',
-      marca:"dogCat",
-      precio: 5.99,
-      descripcion: 'collar para perro raza pequeña 6'
+      img: "./Imageneshtml/IMAGEN 7.jpg",
+      nombre: 'PURINA CAT CHOW',
+      marca:"PURINA",
+      precio: 600,
+      descripcion: 'Bulto de comida para gatos de 24kg'
     },
     {
       id: 7,
-      img: "./Imageneshtml/logoWA.png",
-      nombre: 'Collar 7',
-      marca:"dogCat",
-      precio: 5.99,
-      descripcion: 'collar para perro raza pequeña 7'
+      img: "./Imageneshtml/IMAGEN 8.jpg",
+      nombre: 'SABA DOG',
+      marca:"D&K",
+      precio: 250,
+      descripcion: 'Cobija esponjada para perro raza pequeña o cachorro 70x80'
     },
     {
       id: 8,
-      img: "./Imageneshtml/logoWA.png",
-      nombre: 'Collar 8',
-      marca:"dogCat",
-      precio: 5.99,
-      descripcion: 'collar para perro raza pequeña 8'
+      img: "./Imageneshtml/IMAGEN 9.jpg",
+      nombre: 'PET HAIR',
+      marca:"D&K",
+      precio: 400,
+      descripcion: 'Cepillo electrico con doble funcionalidad, peina y asajea a tu mascota'
     },
     {
       id: 9,
-      img: "./Imageneshtml/logoWA.png",
-      nombre: 'Collar 9',
-      marca:"dogCat",
-      precio: 5.99,
-      descripcion: 'collar para perro raza pequeña 9'
+      img: "./Imageneshtml/IMAGEN 10.jpg",
+      nombre: 'CONFORT-DRI',
+      marca:"OUT-PET",
+      precio: 350,
+      descripcion: 'Pañales para perro cachorro o raza pequeña ultra resistentes 30pz'
     },
     {
       id: 10,
-      img: "./Imageneshtml/logoWA.png",
-      nombre: 'Collar 10',
-      marca:"dogCat",
-      precio: 5.99,
-      descripcion: 'collar para perro raza pequeña 10'
+      img: "./Imageneshtml/IMAGEN 11.jpg",
+      nombre: 'HOODOGI',
+      marca:"DOGGI",
+      precio: 200,
+      descripcion: 'Sueter para perro raza grande, diferentes colores y medidas'
     }
   ];
